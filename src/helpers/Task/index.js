c37cf538e9bf39cef36e0f883b473bde210d0784
junaid aziz <junaidaziz623@gmail.com>
2018-09-21 20:32:42 +0500

    added helps methods to create or update task

--- src/helpers/Task/index.js
+++ src/helpers/Task/index.js
@@ -12,4 +12,45 @@ taskHelpers.createTask = (...props) => {
   return Task.create(...props)
 }
 
+taskHelpers.getTask = (whereCondition) => {
+  return Task.findAll({
+    where: whereCondition
+  })
+}
+
+taskHelpers.findOneTask = (whereCondition) => {
+  return Task.findOne({
+    where: whereCondition
+  })
+}
+
+taskHelpers.updateTask = async (params, taskId) => {
+  const task = await Task.findOne({
+    where: {
+      id: taskId
+    }
+  })
+
+  params = await taskHelpers.mapTaskParams(params, Object.keys(task.dataValues))
+  task.update(params)
+}
+
+taskHelpers.pluckTaskKeys = async (params) => {
+  const task = await Task.findOne()
+  const taskFields = Object.keys(task.dataValues)
+  return taskFields
+}
+
+taskHelpers.mapTaskParams = (params, taskFields) => {
+
+  const taskParams = {}
+  const newKeys = Object.keys(params)
+
+  newKeys.forEach(key => {
+    if (taskFields.includes(key))
+      taskParams[key] = params[key]
+  })
+  return taskParams
+}
+
 module.exports = taskHelpers
